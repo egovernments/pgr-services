3901cf387aafcf38bee2d02103a11d8a9f5c6b8f
Aniket Talele <talele.aniket@gmail.com>
2019-02-11 18:24:18 +0530

    changed status from enum to string

--- rainmaker/tl-services/src/main/java/org/egov/tl/util/TLConstants.java
+++ rainmaker/tl-services/src/main/java/org/egov/tl/util/TLConstants.java
@@ -104,6 +104,18 @@ public class TLConstants {
     public static final String ACTION_PAY  = "PAY";
 
 
+    public static final String STATUS_INITIATED = "INITIATED";
+
+    public static final String STATUS_APPLIED  = "APPLIED";
+
+    public static final String STATUS_APPROVED  = "APPROVED";
+
+    public static final String STATUS_REJECTED  = "REJECTED";
+
+    public static final String STATUS_CANCELLED  = "CANCELLED";
+
+    public static final String STATUS_PAID  = "PAID";
+
 
 
 
